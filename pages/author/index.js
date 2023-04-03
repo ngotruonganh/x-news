d0431425903b79e4a4268eb537dba6d43b1eb600
Lộc Nguyễn <>
2023-04-03 23:49:36 +0700

    [locnt] update folder author and 404.js

---
+++ pages/author/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../../layout'
+export default function AuthorPage() {
+    return (
+        <Layout>
+            <div className="layout-wrapper">
+                hhello
+            </div>
+        </Layout>
+    )
+}
