43d623d09a954562db6cd792b6e26a53678c3d2f
ngotruonganhhhhh <ngotruonganh1901@gmail.com>
2023-04-03 23:48:25 +0700

    fix container

--- components/Header/nav_menu.js
+++ components/Header/nav_menu.js
@@ -1,25 +1,29 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 export default function NavigationMenu({ menu, onClickMenu }) {
-    return (
-        <div className="justify-content-md-center">
-            <ul className="navbar-nav">
-                {menu && menu.map((item, idx) => {
-                    return <li className="nav-item" key={idx}>
-                        <Link
-                            href={{
-                                pathname: item.url,
-                            }}
-                        >
-                            <span className="nav-link color-pink-hover"
-                                onClick={()=>onClickMenu()}
-                            >   
-                                {item.name}
-                            </span>
-                        </Link>
-                    </li>
-                })}
-            </ul>
-        </div>
-    )
+  return (
+    <div className="justify-content-md-center">
+      <ul className="navbar-nav">
+        {menu &&
+          menu.map((item, idx) => {
+            return (
+              <li className="nav-item" key={idx}>
+                <Link
+                  href={{
+                    pathname: item.url,
+                  }}
+                >
+                  <span
+                    className="nav-link color-pink-hover"
+                    onClick={() => onClickMenu()}
+                  >
+                    {item.name}
+                  </span>
+                </Link>
+              </li>
+            );
+          })}
+      </ul>
+    </div>
+  );
 }
